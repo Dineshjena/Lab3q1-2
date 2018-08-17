2a381a1314e1d8568aab641f598ffeca8ed7def3
Dineshjena <42199568+Dineshjena@users.noreply.github.com>
2018-08-17 11:20:59 +0530

    Add files via upload

---
+++ lab3q1.cpp
@@ -0,0 +1,20 @@
+//including library
+#include <iostream>
+using namespace std;
+int main()
+//process
+{
+//declaring variable
+int a=2;
+char b='3';
+float c=4.5;
+double d=43.45;
+bool e=0;
+//size of the variable 
+cout <<"size of int is  "<<sizeof(int)<<" bytes "<<"contents of a = "<<a<<endl;
+cout <<"size of char is "<<sizeof(char)<<" bytes "<<"contents of b = "<<b<<endl;
+cout <<"size of float is "<<sizeof(float)<<" bytes "<<"contents of c = "<<c<<endl;
+cout <<"size of double is "<<sizeof(double)<<" bytes "<<"contents of d = "<<d<<endl;
+cout <<"size of bool is "<<sizeof(bool)<<" bytes "<<"contents of e = "<<e<<endl;
+return 0; 
+}
---
+++ lab3q2.cpp
@@ -0,0 +1,16 @@
+// operating with variables
+#include <iostream>
+using namespace std;
+int main () 
+{ 
+// declaring variables:
+int a=100, b=20;  
+// process:
+cout <<"Numbers are "<<a<<" "<<b<<endl;
+cout <<"Adding the numbers, we get "<<a<<" + "<<b<<" = "<<a+b<<endl;
+cout <<"Subtracting the numbers, we get "<<a<<" - "<<b<<" = "<<a-b<<endl;
+cout <<"Multiplying the numbers, we get "<<a<<" * "<<b<<" = "<<a*b<<endl;
+cout <<"Dividing the numbers, we get "<<a<<" / "<<b<<" = "<<a/b<<endl;
+// terminate the program:
+return 0; 
+} 
